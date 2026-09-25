dadb37230b7c0560d7bb7a7c7675e9d9788fdc78
Mona Abdi <mona.abdi9085@gmail.com>
2026-09-25 13:23:52 +0200

    Add comments clarifying IR sensor setup

--- src_pico/main.py
+++ src_pico/main.py
@@ -4,6 +4,7 @@ from wifi import connect_wifi
 from mqtt import connecting_mqtt, publish_infraredfox_data
 from display import show_ready, show_train_warning, button_pressed
 
+
 # Wifi and MQTT
 print("Connecting to Wifi please wait..")
 connect_wifi()
@@ -17,7 +18,15 @@ try:
 except Exception as error:
     print("MQTT connection failed:", error)
     raise
-
+# Sensors
+# 
+# The system is designed for two IR break-beam sensors:
+# GPIO 2 = warning sensor
+# GPIO 3 = danger sensor
+# IR_Warningsensor = Pin(2, Pin.IN, Pin.PULL_UP)
+# IR_Dangersensor = Pin(3, Pin.IN, Pin.PULL_UP)
+# For the final classroom demo, push buttons were connected to the same GPIO pins
+# to simulate the sensor events and make the demo more reliable.
 
 # Inputs. Buttons will replace IR sensors
 warningzone_button = Pin(2, Pin.IN, Pin.PULL_UP)
@@ -44,7 +53,6 @@ warning_duration = 0  # Changed name into warning for clarity
 
 
 # Functions for BUZZER, ZONE CONTROL and MQTT publishing
-
 
 def alarm(freq, duration, duty):  # LLM USAGE *
     buzzer.freq(freq)
@@ -77,6 +85,7 @@ def set_zone(zone):
         time.sleep(0.15)
         alarm(700, 0.20, 3000)
 
+       
     elif zone == "DANGER":
         if starttime_dangerzone is None:
             starttime_dangerzone = time.ticks_ms()
@@ -84,16 +93,29 @@ def set_zone(zone):
         green_led.value(0)
         warning_led.value(0)
         red_led.value(1)
-
         for i in range(4):
-            alarm(1500, 0.20, 5000)
+            alarm(1500,0.20, 5000)
             time.sleep(0.08)
+                    
+     
+
+    
+
+    
+    
+
+
 
 
 def publish_zone(zone, warning_duration=0, danger_duration=0):
     print("Publishing:", zone)
 
-    publish_infraredfox_data(mqtt_client, zone, warning_duration, danger_duration)
+    publish_infraredfox_data(
+        mqtt_client,
+        zone,
+        warning_duration,
+        danger_duration
+    )
 
 
 ## Button control
@@ -118,6 +140,7 @@ while True:
         show_ready()
         train_detection_active = False
 
+
     # SAFE <-> WARNING
     if warningzone_button.value() == 0:
 
@@ -129,24 +152,37 @@ while True:
 
             set_zone(current_zone)
 
-            publish_zone(current_zone, warning_duration, danger_duration)
+            publish_zone(
+                current_zone,
+                warning_duration,
+                danger_duration
+            )
 
         # WARNING -> SAFE
         elif current_zone == "WARNING":
 
-            warning_duration = (
-                time.ticks_diff(time.ticks_ms(), starttime_warningzone) / 1000
-            )
+            warning_duration = time.ticks_diff(
+                time.ticks_ms(),
+                starttime_warningzone
+            ) / 1000
 
             starttime_warningzone = None
 
-            print("Time in WARNING:", warning_duration, "seconds")
+            print(
+                "Time in WARNING:",
+                warning_duration,
+                "seconds"
+            )
 
             current_zone = "SAFE"
 
             set_zone(current_zone)
 
-            publish_zone(current_zone, warning_duration, danger_duration)
+            publish_zone(
+                current_zone,
+                warning_duration,
+                danger_duration
+            )
 
         # Wait until button is released
         while warningzone_button.value() == 0:
@@ -154,47 +190,67 @@ while True:
 
         time.sleep(0.1)
 
+
     # WARNING <-> DANGER
     elif dangerzone_button.value() == 0:
 
         # WARNING -> DANGER
         if current_zone == "WARNING":
 
-            warning_duration = (
-                time.ticks_diff(time.ticks_ms(), starttime_warningzone) / 1000
-            )
+            warning_duration = time.ticks_diff(
+                time.ticks_ms(),
+                starttime_warningzone
+            ) / 1000
 
             starttime_warningzone = None
 
-            print("Time in WARNING:", warning_duration, "seconds")
+            print(
+                "Time in WARNING:",
+                warning_duration,
+                "seconds"
+            )
 
             current_zone = "DANGER"
 
             set_zone(current_zone)
 
-            publish_zone(current_zone, warning_duration, danger_duration)
+            publish_zone(
+                current_zone,
+                warning_duration,
+                danger_duration
+            )
 
         # DANGER -> WARNING
         elif current_zone == "DANGER":
 
-            danger_duration = (
-                time.ticks_diff(time.ticks_ms(), starttime_dangerzone) / 1000
-            )
+            danger_duration = time.ticks_diff(
+                time.ticks_ms(),
+                starttime_dangerzone
+            ) / 1000
 
             starttime_dangerzone = None
 
-            print("Time in DANGER:", danger_duration, "seconds")
+            print(
+                "Time in DANGER:",
+                danger_duration,
+                "seconds"
+            )
 
             current_zone = "WARNING"
 
             set_zone(current_zone)
 
-            publish_zone(current_zone, warning_duration, danger_duration)
+            publish_zone(
+                current_zone,
+                warning_duration,
+                danger_duration
+            )
 
         # Wait until button is released
         while dangerzone_button.value() == 0:
             time.sleep(0.05)
 
         time.sleep(0.1)
+
 
     time.sleep(0.05)
